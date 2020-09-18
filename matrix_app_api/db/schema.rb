e532295815a95ae14f2eb0f41536b42039921562
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-17 20:55:31 -0400

    failed to seed db

--- matrix_app_api/db/schema.rb
+++ matrix_app_api/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_001929) do
+ActiveRecord::Schema.define(version: 2020_09_18_003007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
-    t.string "descriptiond"
+    t.string "description"
     t.string "img_url"
   end
 
